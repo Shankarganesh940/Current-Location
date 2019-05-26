5f79b837ed6a1c4234c31f442524b9904e77bda1
Shankar Ganesh <shankarganesh940@gmail.com>
2019-05-26 13:54:00 +0530

    Initial Commit

--- Mapping/ViewController.swift
+++ Mapping/ViewController.swift
@@ -5,21 +5,29 @@
 //  Created by Greeens5 on 25/05/19.
 //  Copyright © 2019 Book. All rights reserved.
 //
-
 import UIKit
-
-class ViewController: UIViewController {
-
+import CoreLocation
+class ViewController: UIViewController,CLLocationManagerDelegate {
+    var locationmanager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationmanager.delegate = self
+        locationmanager.desiredAccuracy = kCLLocationAccuracyBest
+        locationmanager.distanceFilter = kCLDistanceFilterNone
+        locationmanager.requestWhenInUseAuthorization()
+        locationmanager.startMonitoringSignificantLocationChanges()
+        locationmanager.startUpdatingLocation()
     }
-
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error", error)
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        print("Location", locations)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
